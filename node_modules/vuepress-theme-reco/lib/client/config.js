f9087c36c40239e360838c2018ba98706a912080
陆志峰 <lzf8686@163.com>
2023-09-04 10:13:03 +0800

    version2

---
+++ node_modules/vuepress-theme-reco/lib/client/config.js
@@ -0,0 +1,13 @@
+import { defineClientConfig } from '@vuepress/client';
+import { applyClientSetup } from './clientSetup';
+import { applyClientEnhance } from './clientEnhance';
+import * as layouts from './layouts';
+export default defineClientConfig({
+    enhance(...args) {
+        applyClientEnhance(...args);
+    },
+    setup() {
+        applyClientSetup();
+    },
+    layouts,
+});
